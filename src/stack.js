458226ed5018c72b331f4d5904864e89bc92c0f8
evgueniazet <zelenkoea19@gmail.com>
2022-10-17 22:32:38 +0300

    fix: stack

--- src/stack.js
+++ src/stack.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+// const { NotImplementedError } = require('../extensions/index.js');
 
 /**
  * Implement the Stack with a given interface via array.
@@ -12,23 +12,88 @@ const { NotImplementedError } = require('../extensions/index.js');
  * stack.pop(); // undefined
  *
  */
- 
+
 
 class Stack {
 
-  
-  push(element) {
-    
-  }  
+  constructor() {
+    this.stack = [];    
+  }
 
-  pop() {
-   
+
+  push(element) {
+    // console.log('this', this);
+    this.stack.push(element);    
+  }
+
+  pop() {    
+    const lastElement = this.stack.pop();
+    // console.log('this.stack', this.stack);
+    // console.log('lastElement', lastElement);
+    return lastElement;
   }
 
   peek() {
-    
+    return this.stack[this.stack.length - 1];
   }
 };
+
+// class StackTest extends Stack {
+//   // constructor() {
+//   //   super();
+//   //   // this.stack = super.stack;    
+//   // }
+
+//   map() {
+//     console.log('map test', this);
+//   }
+// }
+
+// const t = new StackTest();
+// t.push(1);
+// t.map();
+
+// console.log('t.stack', t.stack);
+
+
+
+
+
+// console.log(typeof Stack);
+// let a = new Stack();
+// // console.log('a', a);
+// a.push(1);
+// a.push(2);
+// a.push(5);
+
+// console.log('a1', a);
+
+// const peek = a.peek();
+
+// console.log('peek', peek);
+
+// const pop1 = a.pop();
+// console.log('pop1', pop1);
+
+// const pop2 = a.pop();
+// console.log('pop2', pop2);
+
+// const pop3 = a.pop();
+// console.log('pop3', pop3);
+
+// const pop4 = a.pop();
+// console.log('pop4', pop4);
+// a.pop();
+// a.pop();
+
+// console.log('a2', a);
+// console.log('a.stack', a.stack);
+
+// const b = new Stack();
+// console.log('b', b);
+
+
+
 
 
 
