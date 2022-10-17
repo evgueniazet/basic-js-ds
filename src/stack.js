d410a329dc174410290460c2c66984d8ccf5b6c5
evgueniazet <zelenkoea19@gmail.com>
2022-10-17 16:36:05 +0300

    add: stack solved

--- src/stack.js
+++ src/stack.js
@@ -12,22 +12,25 @@ const { NotImplementedError } = require('../extensions/index.js');
  * stack.pop(); // undefined
  *
  */
+ 
+
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+
+  
+  push(element) {
+    
+  }  
 
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+   
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    
   }
-}
+};
+
+
 
 module.exports = {
   Stack
